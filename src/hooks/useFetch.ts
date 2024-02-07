cdfeb73594433a01a12b2a300a1cdf233aa4aefe
amel-selmane <selmane.amel@gmail.com>
2024-02-07 12:46:57 +0100

    feat: components creation + updates + fetch custom hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url: string) => {
+  const [isData, setIsData] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const FetchData = async () => {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok)
+          throw new Error(`Connexion impossible à l'API ${response.status}`);
+
+        const data = await response.json();
+        setIsData(data);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    FetchData();
+
+    return () => {
+      setIsData("");
+      setIsLoading(true);
+      setIsError(false);
+    };
+  }, [url]);
+
+  return { isData, isLoading, isError };
+};

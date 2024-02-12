4126861e35c8af9e65da57852e608a3a3316ab20
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-02-12 14:15:51 +0100

    (feat): update Filter component + Header component

--- src/components/ToggleButton/index.tsx
+++ src/components/ToggleButton/index.tsx
@@ -1,4 +1,5 @@
 import { useRef, KeyboardEventHandler } from "react";
+import DarkModeIcon from "../../Icon/DarkModeIcon";
 
 const Component = () => {
   const checkbox = useRef<HTMLInputElement>(null);
@@ -13,6 +14,7 @@ const Component = () => {
 
   return (
     <>
+      <DarkModeIcon kind="sun" />
       <input
         type="checkbox"
         id="check-button"
@@ -25,8 +27,9 @@ const Component = () => {
         htmlFor="check-button"
         tabIndex={0}
         onKeyDown={handleKeydown}
-        className="check-button relative h-5 w-[40px] cursor-pointer select-none rounded-[10px] bg-white before:absolute before:left-[7%] before:top-2/4 before:size-[14px] before:translate-y-[-50%] before:rounded-3xl before:bg-royalBlue before:content-[''] [&:before]:transition-transform [&:before]:duration-300"
+        className="check-button relative h-5 w-[40px] cursor-pointer select-none self-center rounded-[10px] bg-white before:absolute before:left-[7%] before:top-2/4 before:size-[14px] before:translate-y-[-50%] before:rounded-3xl before:bg-royalBlue before:content-[''] [&:before]:transition-transform [&:before]:duration-300"
       ></label>
+      <DarkModeIcon kind="moon" />
     </>
   );
 };

1af3ee84601c9a2161c3d685f6f90a307cc62152
amel-selmane <selmane.amel@gmail.com>
2024-02-13 12:02:10 +0100

    adding route details + logoBackground

--- src/utils/ExtractHSLValue/index.ts
+++ src/utils/ExtractHSLValue/index.ts
@@ -1,19 +1,24 @@
 const utils = (hslString: string) => {
   const regex = /hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/;
-  const matches = hslString.match(regex);
-  //hsl(15, 19, 19) devient => ["hsl(15, 19, 19)", "15", "19", "19"]
-  //si les 4 élements sont bien présents, on a un pattern hsl valide et donc on peut
-  //récupérer hue, saturation et lightness
 
-  if (matches && matches.length === 4) {
-    const hue = Number(matches[1]);
-    const saturation = Number(matches[2]);
-    const lightness = Number(matches[3]);
+  if (regex.test(hslString)) {
+    const matches = hslString.match(regex);
+    //hsl(15, 19, 19) devient => ["hsl(15, 19, 19)", "15", "19", "19"]
+    //si les 4 élements sont bien présents, on a un pattern hsl valide et donc on peut
+    //récupérer hue, saturation et lightness
 
-    return { hue, saturation, lightness };
+    if (matches && matches.length === 4) {
+      const hue = Number(matches[1]);
+      const saturation = Number(matches[2]);
+      const lightness = Number(matches[3]);
+
+      return { hue, saturation, lightness };
+    }
+  } else {
+    console.error(
+      "HSL value is incorrect or missing in data logoBackground value",
+    );
   }
-
-  return;
 };
 
 export default utils;

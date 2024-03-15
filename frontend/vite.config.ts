bbfb87f6595908e4d07adeb5db4c205820a4f720
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-03-15 15:52:48 +0100

    (feat): refactoring dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    strictPort: true
+  },
 });
